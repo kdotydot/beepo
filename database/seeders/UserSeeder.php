24e4630af25e429c4820e77983d1559dc5295fe9
Kyron Joves <kyron.joves@gmail.com>
2020-12-14 01:55:22 +0800

    initial commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Beepo User',
+            'email' => 'demo@demo.com.au',
+            'password' => Hash::make('hello2020'),
+        ]);
+    }
+}
